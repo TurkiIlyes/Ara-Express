7a1a14c94b01a0b701bac8e7c2e028a60d6c5a4a
ilyess.torki.v <ilyess.torki.v@gmail.com>
2024-08-16 05:06:15 +0000

    updae the project

---
+++ src/routes/emailRoute.ts
@@ -0,0 +1,31 @@
+// import express from "express";
+// import rateLimit from "express-rate-limit";
+// const router = express.Router();
+
+// import {
+//   contactUs,
+// } from "../services/authService";
+// import {
+//     contactUsValidator,
+// } from "../utils/validators/authValidator";
+
+// // Rate limiter middleware to prevent brute-force attacks
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 200, // limit each IP to 100 requests per windowMs
+//   message: "Too many requests from this IP, please try again after 15 minutes",
+// });
+
+// router.use(limiter);
+// /**
+//  * @route   POST /sign-up
+//  * @desc    Register a new user
+//  * @access  Public
+//  */
+// router.post(
+//   "/",
+//   contactUsValidator,
+//   contactUs
+// );
+
+// export default router;
